0f1096ff243f542d1a6df63fce908f748e295a35
daniel <daniel.febryanto@live.com>
2016-08-23 11:50:20 +0700

    .
    .

---
+++ application/controllers/home.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+  function __construct(){
+          parent::__construct();
+          //$this->load->library(array('template'));
+          $this->load->model(array('homeModel'));//load testCrudmodel dari folder model
+      }
+  public function index()
+  {
+    $this->load->view("index");
+  }
+    function testRegis(){
+      $value = array('nama'=>$_POST['nama']);
+      $this->homeModel->registerInsert($value);
+    }
+  function loginMember(){
+    $value = array('username'=>$_POST['username'],'password'=>$_POST['password']);
+    $oper = $this->homeModel->memberLogin($value);
+    if($oper == true){
+      $this->load->view("index");
+    }else {
+      $this->session->set_flashdata("errror","Username Password salah");
+      $this->load->view("index");
+    }
+  }
+}
---
+++ application/models/homeModel.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HomeModel extends CI_Model {
+
+
+  function registerInsert($value)
+  {
+    $insert = $this->db->insert('member', $value);
+
+    if ($insert) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+
+  function memberLogin(){
+    $check = $this->db->select_where('member', $value);
+
+    if ($check->num_rows() > 0) {
+      foreach ($check->result_array() as $row) {
+        $value = array('username'=>$row['nama'],'password'=>$row['password'],'userid'=>$row['id_member']);
+        $this->session->set_userdata($value);
+      }
+      return true;
+    }
+    return false;
+
+  }
+
+
+}
